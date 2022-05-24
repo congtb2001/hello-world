76e051178a07dfaa846abaf33b09dce748dfea4c
congtb2001 <tranbacong123@gmail.com>
2022-05-24 11:29:28 +0700

    1st lan dau lam chuyen ay - upload to github

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world;
+
+/**
+ *
+ * @author SE150441 Tran Ba Cong
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to SE1620. GitHub is so funny!!!");
+    }
+    
+}
